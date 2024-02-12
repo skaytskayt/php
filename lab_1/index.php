0ea997625ce078d135acfa74c731c02160a137c9
Kirill Lopaev <lopaevkirill@MacBook-Air-Kirill.local>
2024-02-12 19:39:51 +0300

    Первая лаб работа

---
+++ lab_1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="/php/lab_1/style.css">
+</head>
+<body>
+  <header><img src="/php/lab_1/img/images.png" alt="Лого МосПолитех">
+  <h1>Домашняя работа: Hello, World!</h1></header>
+  <main>
+  <?php echo 'Hello, World'; ?>
+  </main>
+  <footer></footer>
+</body>
+</html>
