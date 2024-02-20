241caf04326be13a967bdf78641fd12b6421b640
Kirill Lopaev <lopaevkirill@MacBook-Air-Kirill.local>
2024-02-20 13:13:26 +0300

    forma

---
+++ lab_3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        get_headers('https://docs.google.com/spreadsheets/d/1wo72S6p5xXMA93kFcQlJh0pAclYphX3WimbCmqRzWJI/edit#gid=1384460340');
+    ?>
+    <a href="index.html">Переход на 1 страницу</a>
+
+</body>
+</html>
